5fe69373da856a43f548cfb8f401c520a7b06526
Simo Nurmi <simo.nurmi@solita.fi>
2018-10-30 17:08:48 +0200

    Complete exercise 4

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,58 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        values {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit rank)
+      (Integer/parseInt (str rank))
+      (values rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (let [sorted (sort (vals (frequencies (map rank hand))))]
+    (= [2 3] sorted)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [sorted (sort (vals (frequencies (map rank hand))))]
+    (or (= [1 2 2] sorted) (= [1 4] sorted))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+        low-ace (sort (replace {14 1} sorted))
+        fst (first sorted)
+        lst (last sorted)
+        fst-low (first low-ace)
+        lst-low (last low-ace)]
+    (or (= sorted (range fst (+ 1 lst))) (= low-ace (range fst-low (+ 1 lst-low))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        check (fn [checker] ((first checker) hand))
+        hands (filter check checkers)
+        scores (map second hands)]
+    (apply max scores)))
